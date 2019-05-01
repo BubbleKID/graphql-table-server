6c2ab1be136b7c6c0dae6f1b0e0142884cc3d5e2
Mark <fqchenxin@gmail.com>
2019-05-01 22:55:26 +1000

    fixed port bug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import tradeSchema from './schema/tradeSchema';
 import withdrawSchema from './schema/withdrawSchema';
 import cors from 'cors';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 //allow cross-origin requests
